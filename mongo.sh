77ba75c4fc9c6719f70a66d1883a33bc8a997d74
Daniele Muffato <danixgithub1@gmail.com>
2025-05-31 05:40:39 +0200

    auto launch containers

--- mongo.sh
+++ mongo.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# to start the docker container with the server:
-# 1) podman run -d -v mongodb_data:/data/db --name mongo-server --network none docker.io/library/mongo:latest
-# 2) podman run -d -v mongodb_data:/data/db --name mongo-server -p27017:27017 docker.io/library/mongo:latest 
-# note: 1) completely isolates the server, 2) if other local apps needs to access the server
+# ./mongo.sh <query_file> [<url>]
 
 QUERY_FILE="$1"
 DB_URL="$2"
 
 ! [[ -f "$QUERY_FILE" ]] && echo "not a file: $1" && exit 1
 
+if [[ -z "$(podman port mongo-server)" && -n "$DB_URL" ]] || [[ -n "$(podman port mongo-server)" && -z "$DB_URL" ]]; then
+    echo 'stopping mongo server...'
+    podman stop mongo-server >/dev/null
+fi
+
 if [[ "$(podman ps --filter "name=mongo-server" --format "{{.ID}}" | wc -l)" -eq 0 ]]; then
-    echo "container 'mongo-server' is not running"
-    exit 1
+    if [[ -z "$DB_URL" ]]; then
+        echo "launching mongo server..."
+        podman run --rm -d -v mongodb_data:/data/db --name mongo-server --network none docker.io/library/mongo:latest >/dev/null
+    else
+        echo "launching mongo server [WARNING: exposing port on localhost]..."
+        podman run --rm -d -v mongodb_data:/data/db --name mongo-server -p27017:27017 docker.io/library/mongo:latest >/dev/null
+    fi
+    sleep 1
 fi
 
 echo "$QUERY_FILE" | entr sh -c "clear && podman exec -t mongo-server mongosh $DB_URL --quiet --eval \"\$(cat $QUERY_FILE)\""
-

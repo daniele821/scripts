415873c98d9ed33053f76d794658ba5d87696d3b
Daniele Muffato <danixgithub1@gmail.com>
2025-07-06 19:59:43 +0200

    remove useless build script

--- images/oracle-client/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
-
-podman build -t custom-sqlplus -f "${SCRIPT_DIR}/Dockerfile" "${SCRIPT_DIR}" 

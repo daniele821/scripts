147640ee9924f28c13ba4efe7de9b0ee14ac6080
Daniele Muffato <danixgithub1@gmail.com>
2025-07-09 08:48:52 +0200

    added check on deps

--- images/neovim/run.sh
+++ images/neovim/run.sh
@@ -8,6 +8,10 @@
 # - creates podman image named neovim, if missing
 # - runs that image and mounts current directory inside of it
 
+for dep in podman dirname realpath; do
+    ! command -v "$dep" &>/dev/null && echo "Missing dependency: $dep" && exit 1
+done
+
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 SCRIPT_DIRNAME="$(basename "$(realpath "$PWD")")"
 [ "$SCRIPT_DIRNAME" == "/" ] && SCRIPT_DIRNAME='host'
